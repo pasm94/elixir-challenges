a48729799ff2dfdfa4bc154406948047e2c077de
Paulo A. S. Müller <paulomuller94@gmail.com>
2021-09-19 15:25:44 -0300

    Relationship user - meal created

--- desafio-crud-refeicoes-diarias/lib/exmeal/meal.ex
+++ desafio-crud-refeicoes-diarias/lib/exmeal/meal.ex
@@ -3,16 +3,21 @@ defmodule Exmeal.Meal do
 
   import Ecto.Changeset
 
+  alias Exmeal.User
+
   @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
 
-  @required_params [:description, :date, :calories]
+  @required_params [:description, :date, :calories, :user_id]
 
-  @derive {Jason.Encoder, only: [:id, :description, :date, :calories]}
+  @derive {Jason.Encoder, only: [:id, :description, :date, :calories, :user_id]}
 
   schema "meals" do
     field(:description, :string)
     field(:date, :date)
     field(:calories, :integer)
+
+    belongs_to(:user, User)
 
     timestamps()
   end
---
+++ desafio-crud-refeicoes-diarias/lib/exmeal/user.ex
@@ -0,0 +1,31 @@
+defmodule Exmeal.User do
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  alias Exmeal.Meal
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  @required_params [:name, :cpf, :email]
+
+  @derive {Jason.Encoder, only: [:id, :name, :cpf, :email]}
+
+  schema "users" do
+    field(:name, :string)
+    field(:cpf, :string)
+    field(:email, :string)
+
+    has_many(:meals, Meal)
+
+    timestamps()
+  end
+
+  def changeset(struct \\ %__MODULE__{}, params) do
+    struct
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:cpf, is: 11)
+    |> validate_format(:email, ~r/@/)
+  end
+end
